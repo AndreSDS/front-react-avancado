a539762fbc3d6da65ed99cbf62ef783e913dfa2c
AndreSDS <andre_smiths@outlook.com>
2021-09-08 11:47:46 -0300

    fazendo ajustes

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,3 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
 
+console.log('process.env.GRAPHQL_HOST ', process.env.GRAPHQL_HOST)
+
 export const client = new GraphQLClient(process.env.GRAPHQL_HOST)
